2e09c5368bb61783f856e0905c70dc8741c20b6f
Daniel Hollands <daniel@limeblast.co.uk>
2015-01-31 18:48:08 +0000

    I don't know what I've done, but hopefully it'll help

--- src/js/boot.js
+++ src/js/boot.js
@@ -12,18 +12,6 @@
     create: function () {
       this.game.input.maxPointers = 1;
 
-      if (this.game.device.desktop) {
-        this.game.scale.pageAlignHorizontally = true;
-      } else {
-        this.game.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        this.game.scale.minWidth =  480;
-        this.game.scale.minHeight = 260;
-        this.game.scale.maxWidth = 800;
-        this.game.scale.maxHeight = 600;
-        this.game.scale.forceLandscape = true;
-        this.game.scale.pageAlignHorizontally = true;
-        this.game.scale.setScreenSize(true);
-      }
       this.game.state.start('preloader');
     }
   };
